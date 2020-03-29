f0157c07edd947d639be7678fa6ec98a32571386
Breno Zupeli <breno.zupeli@intelliway.com.br>
2020-03-29 19:42:03 -0300

    validação e TDD

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,6 @@ const connection = require('./../database/connection');
 
 module.exports = {
     async index(req, res) {
-        console.log('get incidents')
         const { page = 1 } = req.query;
 
         const [count] = await connection('incidents').count();
---
+++ backend/src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(3333, _ => {
+    console.log('Server running!')
+});
